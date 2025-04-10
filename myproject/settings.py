ee97cc1ed3fc99ddc9e7dbc26fad9c8a1d8f1d80
oboyle3 <po720999@sju.edu>
2025-04-09 20:53:39 -0400

    bootstrap

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-oibcjs3s-5(*sf-r@l(e(u^rca#z)o5h9&!t)u3io4ddyc15-8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.0.0.96']
 
 
 # Application definition
